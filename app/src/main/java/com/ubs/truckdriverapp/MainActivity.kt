3c4ce2d97dbd210af3c7c0863f337dc9247cb298
Martin Ubaque <m.ubaque@uniandes.edu.co>
2023-09-30 19:43:49 -0500

    Maps Api incluida

--- app/src/main/java/com/ubs/truckdriverapp/MainActivity.kt
+++ app/src/main/java/com/ubs/truckdriverapp/MainActivity.kt
@@ -2,10 +2,18 @@ package com.ubs.truckdriverapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonClick = findViewById<Button>(R.id.botonMapa)
+        buttonClick.setOnClickListener {
+            val intent = Intent(this, FuncAF::class.java)
+            startActivity(intent)
+        }
     }
 }
